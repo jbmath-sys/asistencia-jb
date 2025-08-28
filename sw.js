3ebc3dcab1d4a63c88de630d912703a35fd973cd
Juan Bernardo Lopez Acosta <jblopezacosta@gmail.com>
2025-08-27 19:22:23 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "asistencia-jb-v1";
+const CACHE_NAME = "asistencia-jb-v2";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -8,17 +8,13 @@ const APP_SHELL = [
 ];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k !== CACHE_NAME) && caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE_NAME) && caches.delete(k))))
   );
   self.clients.claim();
 });
@@ -26,9 +22,7 @@ self.addEventListener("activate", (e) => {
 self.addEventListener("fetch", (e) => {
   const url = new URL(e.request.url);
   if (url.origin === self.location.origin) {
-    e.respondWith(
-      caches.match(e.request).then((cached) => cached || fetch(e.request))
-    );
+    e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request)));
   } else {
     e.respondWith(
       fetch(e.request).then((res) => {
